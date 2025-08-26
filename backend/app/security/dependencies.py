9ffdf1da14210ef48d3fbab3300deda8ba05ce17
“Mitchel <mitchelonyebuchi@gmail.com>
2025-08-25 20:30:38 -0500

    fix(security): correct formatting of HTTPException in get_current_user function

--- backend/app/security/dependencies.py
+++ backend/app/security/dependencies.py
@@ -14,6 +14,20 @@ def get_user(db: Session, username: str):
 
 def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
     credentials_exception = HTTPException(
-        status_code= status.HTTP_401_UNAUTHORIZED
-        detail= "Could not validate"
+        status_code= status.HTTP_401_UNAUTHORIZED,
+        detail= "Could not validate",
+        headers= {"WWW-Authenticate": "Bearer"}
     )
+    try:
+        payload= jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    
+    user = get_user(db, username = username)
+    if user is None:
+        raise credentials_exception
+    return user
+    
